ad8dd6cfa97f8e83f8bf66c1cdf0f668ef948969
Anilbal <anilbal7676@gmail.com>
2024-07-15 19:11:46 +0545

    completed temperature app

--- 25_temperature_converter/app.js
+++ 25_temperature_converter/app.js
@@ -1,6 +1,11 @@
 let cels=document.getElementById("celsius")
 let fah=document.getElementById("fahrenheit")
 
-cels.addEventListener("change",(e)=>{
-    console.log(e.target.value)
-})
+function celToFir(){
+    let output=(parseFloat(cels.value)*9/5+32)
+    fah.value=parseFloat(output.toFixed(2));
+}
+function firToCel(){
+    let output=(parseFloat(fah.value)-32)*5/9;
+    cels.value=parseFloat(output.toFixed(2));
+}
